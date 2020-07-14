e06175a198fb78ca411c7d0d35005561c2309581
gaetan.pagnerre <gaetan.pagnerre@gmail.com>
2020-07-14 18:44:11 +0200

    add constructor

--- tennis-game/src/main/java/org/highfi/gpag/tennisgame/Score.java
+++ tennis-game/src/main/java/org/highfi/gpag/tennisgame/Score.java
@@ -1,12 +1,11 @@
 package org.highfi.gpag.tennisgame;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Score {
     private int scorePlayer1;
